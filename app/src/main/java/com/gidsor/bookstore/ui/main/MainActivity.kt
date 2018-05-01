3b0f37dfdeb9ac169e4bdf6fa7678d947a90da33
Vadim Denisov <gidsor@gmail.com>
2018-05-01 23:31:35 +0500

    disable shift mode and fix hidden text

--- app/src/main/java/com/gidsor/bookstore/ui/main/MainActivity.kt
+++ app/src/main/java/com/gidsor/bookstore/ui/main/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.gidsor.bookstore.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.design.internal.BottomNavigationItemView
+import android.support.design.internal.BottomNavigationMenuView
 import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import com.gidsor.bookstore.*
@@ -12,9 +13,14 @@ import com.gidsor.bookstore.ui.account.AccountFragment
 import com.gidsor.bookstore.ui.genre.GenreFragment
 import com.gidsor.bookstore.ui.purchase.PurchaseFragment
 import com.gidsor.bookstore.ui.store.StoreFragment
+import java.lang.reflect.AccessibleObject.setAccessible
+import java.lang.reflect.Array.setBoolean
+import java.lang.reflect.Field
+
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
+    @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +29,25 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
 
         val navigationView: BottomNavigationView = findViewById(R.id.navigation)
         navigationView.setOnNavigationItemSelectedListener(this)
+
+        // always show text in navigation for more 3 elements and disable shift mode
+        navigationView.disableShiftMode()
+
+    }
+
+    @SuppressLint("RestrictedApi")
+    fun BottomNavigationView.disableShiftMode() {
+        val menuView = getChildAt(0) as BottomNavigationMenuView
+        val shiftingMode = menuView::class.java.getDeclaredField("mShiftingMode")
+        shiftingMode.isAccessible = true
+        shiftingMode.setBoolean(menuView, false)
+        shiftingMode.isAccessible = false
+        for (i in 0 until menuView.childCount) {
+            val item = menuView.getChildAt(i) as BottomNavigationItemView
+            item.setShiftingMode(false)
+            // set once again checked value, so view will be updated
+            item.setChecked(item.itemData.isChecked)
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
