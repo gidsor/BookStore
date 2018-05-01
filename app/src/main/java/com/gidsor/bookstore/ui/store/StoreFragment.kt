a23013b9e663a0494d889f12da59886cdb52fa10
Vadim Denisov <gidsor@gmail.com>
2018-05-01 22:17:26 +0500

    fix work TabLayout in Fragment

--- app/src/main/java/com/gidsor/bookstore/ui/store/StoreFragment.kt
+++ app/src/main/java/com/gidsor/bookstore/ui/store/StoreFragment.kt
@@ -17,7 +17,17 @@ class StoreFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_everything, container, false)
+        return inflater.inflate(R.layout.fragment_store, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewPager = view.findViewById(R.id.store_view_pager)
+        pagerAdapter = TabsPagerAdapter(childFragmentManager)
+        viewPager.adapter = pagerAdapter
+        tabLayout = view.findViewById(R.id.tabs)
+        tabLayout.setupWithViewPager(viewPager)
+
+    }
 }
