df357cd90179a47a7217c4e2f03ebd773ac9bedf
Vadim Denisov <gidsor@gmail.com>
2018-05-05 23:40:11 +0500

    more info

--- app/src/main/java/com/gidsor/bookstore/data/model/Book.kt
+++ app/src/main/java/com/gidsor/bookstore/data/model/Book.kt
@@ -1,3 +1,4 @@
 package com.gidsor.bookstore.data.model
 
-class Book(val name: String, val image: Int)
+class Book(val name: String, val image: Int, val authors: String = "", val year: Int = 2001,
+           val publisher: String = "", val description: String = "")
