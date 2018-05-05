583203f8a6910c27c1995f380ccbc09f8fc852c2
Vadim Denisov <gidsor@gmail.com>
2018-05-05 23:47:16 +0500

    test data array while no api

---
+++ app/src/main/java/com/gidsor/bookstore/ui/bookitem/BookTestArrayData.kt
@@ -0,0 +1,14 @@
+package com.gidsor.bookstore.ui.bookitem
+
+import com.gidsor.bookstore.R
+
+class BookTestArrayData {
+    companion object {
+        val names: Array<String> = arrayOf("Тонкое искусство пофигизма", "Ходячий замок", "Оно",
+                "Книга пыли. Прекрасная дикарка", "Верховная Мать Змей", "Совершенный код", "Потерянное озеро")
+        val images: Array<Int> = arrayOf(R.drawable.book1, R.drawable.book2, R.drawable.book3,
+                R.drawable.book4, R.drawable.book5, R.drawable.book6, R.drawable.book7)
+        val genres: Array<String> = arrayOf("Тонкое искусство пофигизма", "Ходячий замок", "Оно",
+                "Книга пыли. Прекрасная дикарка", "Верховная Мать Змей", "Совершенный код", "Потерянное озеро")
+    }
+}
