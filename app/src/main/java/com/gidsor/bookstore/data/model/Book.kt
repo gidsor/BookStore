c6700ad086b26e0b840957672a6b5ffd0abd38ba
Vadim Denisov <gidsor@gmail.com>
2018-05-05 23:52:55 +0500

    test data while no api

--- app/src/main/java/com/gidsor/bookstore/data/model/Book.kt
+++ app/src/main/java/com/gidsor/bookstore/data/model/Book.kt
@@ -1,4 +1,4 @@
 package com.gidsor.bookstore.data.model
 
-class Book(val name: String, val image: Int, val authors: String = "", val year: Int = 2001,
+class Book(val name: String, val image: Int, val genre: String = "", val authors: String = "", val year: Int = 2001,
            val publisher: String = "", val description: String = "")
