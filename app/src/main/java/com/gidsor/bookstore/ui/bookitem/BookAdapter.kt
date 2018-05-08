0b702640352cb1116cf253e7de53415af9774076
Vadim Denisov <gidsor@gmail.com>
2018-05-08 16:31:13 +0500

    added info about books

--- app/src/main/java/com/gidsor/bookstore/ui/bookitem/BookAdapter.kt
+++ app/src/main/java/com/gidsor/bookstore/ui/bookitem/BookAdapter.kt
@@ -12,18 +12,28 @@ import com.gidsor.bookstore.R
 import com.gidsor.bookstore.data.model.Book
 
 class BookAdapter(val context: Context, val bookItems: ArrayList<Book>) : BaseAdapter() {
+
     override fun getView(position: Int, _convertView: View?, parent: ViewGroup?): View {
         var convertView: View? = _convertView
         if (convertView == null) {
             val mInflater: LayoutInflater = context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            convertView = mInflater.inflate(R.layout.fragment_book_item, null)
+            convertView = mInflater.inflate(R.layout.fragment_book_item, null)!!
         }
-        val imageBook: ImageView = convertView!!.findViewById(R.id.book_image)
-        val nameGenre: TextView = convertView!!.findViewById(R.id.book_name)
 
-        val bookPos: Book = bookItems[position]
-        imageBook.setImageResource(bookPos.image)
-        nameGenre.text = bookPos.name
+        val imageBook: ImageView = convertView.findViewById(R.id.book_image)
+        val nameBook: TextView = convertView.findViewById(R.id.book_name)
+        val authorsBook: TextView = convertView.findViewById(R.id.book_authors)
+        val yearBook: TextView = convertView.findViewById(R.id.book_year)
+        val genreBook: TextView = convertView.findViewById(R.id.book_genre)
+        val descriptionBook: TextView = convertView.findViewById(R.id.book_description)
+
+        val book: Book = bookItems[position]
+        imageBook.setImageResource(book.image)
+        nameBook.text = book.name
+        authorsBook.text = "Автор: " + book.authors
+        yearBook.text = "Год издания: " + book.year.toString()
+        genreBook.text = "Жанр: " + book.genre
+        descriptionBook.text = book.description
 
         return convertView
     }
--- app/src/main/java/com/gidsor/bookstore/ui/everything/EverythingFragment.kt
+++ app/src/main/java/com/gidsor/bookstore/ui/everything/EverythingFragment.kt
@@ -1,7 +1,6 @@
 package com.gidsor.bookstore.ui.everything
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.support.v4.app.ListFragment
 import android.view.LayoutInflater
 import android.view.View
@@ -10,14 +9,11 @@ import android.widget.Toast
 import com.gidsor.bookstore.R
 import com.gidsor.bookstore.data.model.Book
 import com.gidsor.bookstore.ui.bookitem.BookAdapter
+import com.gidsor.bookstore.ui.bookitem.BookTestArrayData
 
 class EverythingFragment : ListFragment() {
-    private val names: Array<String> = arrayOf("Тонкое искусство пофигизма", "Ходячий замок", "Оно",
-            "Книга пыли. Прекрасная дикарка", "Верховная Мать Змей", "Совершенный код", "Потерянное озеро")
-    private val images: Array<Int> = arrayOf(R.drawable.book1, R.drawable.book2, R.drawable.book3,
-            R.drawable.book4, R.drawable.book5, R.drawable.book6, R.drawable.book7)
 
-    private lateinit var genreItems: ArrayList<Book>
+    private lateinit var bookItems: ArrayList<Book>
     private lateinit var adapter: BookAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,14 +23,15 @@ class EverythingFragment : ListFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        genreItems = ArrayList()
-        for (i in 0 until names.count()) {
-            genreItems.add(Book(names[i], images[i]))
-        }
-        adapter = BookAdapter(view!!.context, genreItems)
+        bookItems = BookTestArrayData.books
+//        bookItems = ArrayList()
+//        for (i in 0 until names.count()) {
+//            bookItems.add(Book(names[i], images[i]))
+//        }
+        adapter = BookAdapter(view!!.context, bookItems)
         listAdapter = adapter
         listView.setOnItemClickListener { parent, view, position, id ->
-            Toast.makeText(activity, names[position], Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, bookItems[position].name, Toast.LENGTH_SHORT).show()
         }
     }
 }
