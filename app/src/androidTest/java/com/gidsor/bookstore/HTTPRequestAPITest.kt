13fdf98434174da6dda063696e4983f186bb71bc
Vadim Denisov <gidsor@gmail.com>
2018-05-20 20:52:51 +0500

    added api test

---
+++ app/src/androidTest/java/com/gidsor/bookstore/HTTPRequestAPITest.kt
@@ -0,0 +1,14 @@
+package com.gidsor.bookstore
+
+import android.support.test.InstrumentationRegistry
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class HTTPRequestAPITest {
+    @Test
+    fun getStatus() {
+        assertEquals("ok", "owwk")
+    }
+}
