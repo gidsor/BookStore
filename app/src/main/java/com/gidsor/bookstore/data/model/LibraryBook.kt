44d620ef159c1dc9059e89331c7afe4df9ae1af7
Vadim Denisov <gidsor@gmail.com>
2018-05-21 22:16:23 +0500

    -

--- app/src/main/java/com/gidsor/bookstore/data/model/LibraryBook.kt
+++
@@ -1,4 +0,0 @@
-package com.gidsor.bookstore.data.model
-
-class LibraryBook(val name: String, val image: Int, val genre: String = "", val authors: String = "", val year: Int = 2001,
-           val publisher: String = "", val description: String = "", val price: Int = 1234)
