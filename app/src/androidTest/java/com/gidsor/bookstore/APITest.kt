31d73deca208313ae0b8fa0bf67105b28c6e5f70
Vadim Denisov <gidsor@gmail.com>
2018-05-27 12:54:01 +0500

    refactoring

--- app/src/androidTest/java/com/gidsor/bookstore/APITest.kt
+++ app/src/androidTest/java/com/gidsor/bookstore/APITest.kt
@@ -9,24 +9,4 @@ class APITest {
     fun status() {
         Assert.assertEquals("{\"status\":\"ok\"}", HTTPRequestAPI.status().toString())
     }
-
-    @Test
-    fun login() {
-        Assert.assertEquals("{\"status\":\"ok\",\"id\":1}", HTTPRequestAPI.login("login1", "pass1").toString())
-    }
-
-    @Test
-    fun library() {
-        Assert.assertEquals("{\"status\":\"ok\",\"result\":[{\"status\":\"Прочитано\",\"book\":\"Мастер и Маргарита\",\"author\":\"Михаил Афанасьевич Булгаков\",\"lib\":\"Валентина Шукшина\"},{\"status\":\"Прочитано\",\"book\":\"И грянул гром\",\"author\":\"Рэй Бредбери\",\"lib\":\"Валентина Шукшина\"}]}", HTTPRequestAPI.library("1").toString())
-    }
-
-    @Test
-    fun registration() {
-        Assert.assertEquals("{\"status\":\"ok\"}", HTTPRequestAPI.registration("qazwsx@mail.ru", "qazwsx").toString())
-    }
-
-    @Test
-    fun find() {
-        Assert.assertEquals("{\"status\":\"ok\",\"id\":[[8,\"Черный человек\",\"Сергей Александрович Есенин\",\"Поэзия\",\"Художественная литература\",\"Русский\"]]}", HTTPRequestAPI.composition(author = "Есенин").toString())
-    }
 }
--- app/src/test/java/com/gidsor/bookstore/ExampleTest.kt
+++ app/src/test/java/com/gidsor/bookstore/ExampleTest.kt
@@ -1,11 +1,12 @@
 package com.gidsor.bookstore
 
+import com.gidsor.bookstore.data.network.HTTPRequestAPI
 import org.junit.Assert
 import org.junit.Test
 
 class ExampleTest {
     @Test
-    fun simpleTest() {
-        Assert.assertEquals("4", (2 + 2).toString())
+    fun status() {
+        Assert.assertEquals("{\"status\":\"ok\"}", HTTPRequestAPI.status().toString())
     }
 }
