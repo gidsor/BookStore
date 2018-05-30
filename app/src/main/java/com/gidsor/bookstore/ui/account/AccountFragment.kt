b78355ea1a97302097614bf2a02a69f94120cefd
Vadim Denisov <gidsor@gmail.com>
2018-05-30 23:57:15 +0500

    added images to library of user

--- app/src/main/java/com/gidsor/bookstore/ui/account/AccountFragment.kt
+++ app/src/main/java/com/gidsor/bookstore/ui/account/AccountFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.gidsor.bookstore.R
@@ -17,6 +18,7 @@ import com.gidsor.bookstore.data.database.OrderOfUserArrayData
 import com.gidsor.bookstore.data.model.User
 import com.gidsor.bookstore.data.network.DelFromLibraryTask
 import com.gidsor.bookstore.data.network.GetOrderTask
+import com.squareup.picasso.Picasso
 
 class AccountFragment : Fragment() {
 
@@ -42,41 +44,99 @@ class AccountFragment : Fragment() {
             }
         }
 
-        fun updateLibraryOfUser(currentUser: User, view: View) {
-            val library: LinearLayout = view.findViewById(R.id.account_library)
-            library.removeAllViewsInLayout()
-            LibraryArrayData.updateCompositions(currentUser)
-            for (i in LibraryArrayData.getCompositions()) {
-                val newTextView = TextView(view.context)
-                val paramsOfTextView = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-                paramsOfTextView.setMargins(10, 10, 0, 0)
-                newTextView.layoutParams = paramsOfTextView
+//        fun updateLibraryOfUser(currentUser: User, view: View) {
+//            val library: LinearLayout = view.findViewById(R.id.account_library)
+//            library.removeAllViewsInLayout()
+//            LibraryArrayData.updateCompositions(currentUser)
+//            for (i in LibraryArrayData.getCompositions()) {
+//                val newTextView = TextView(view.context)
+//                val paramsOfTextView = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+//                paramsOfTextView.setMargins(10, 10, 0, 0)
+//                newTextView.layoutParams = paramsOfTextView
+//
+//                val bookName = i.title
+//                val bookAuthor = i.author
+//
+//                val removeButton = Button(view.context)
+//                val paramsOfButton = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+//                paramsOfButton.setMargins(10, 10, 0, 0)
+//                removeButton.layoutParams = paramsOfTextView
+//                removeButton.text = "Удалить из библиотеки"
+//                removeButton.setOnClickListener { v ->
+//                    DelFromLibraryTask().execute(user.id.toString(), i.lib, i.composition)
+//                    updateLibraryOfUser(user, view)
+//                }
+//                when {
+//                    i.lib == "1" -> {
+//                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Прочитанное"
+//                    }
+//                    i.lib == "2" -> {
+//                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Отложенное"
+//                    }
+//                    i.lib == "3" -> {
+//                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Избранное"
+//                    }
+//                }
+//                library.addView(newTextView)
+//                library.addView(removeButton)
+//            }
+//            if (LibraryArrayData.getCompositions().size == 0) {
+//                val newTextView = TextView(view.context)
+//                val paramsOfTextView = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+//                paramsOfTextView.setMargins(10, 10, 0, 0)
+//                newTextView.layoutParams = paramsOfTextView
+//                newTextView.text = "Пусто"
+//                library.addView(newTextView)
+//            }
+//        }
 
+        fun updateLibraryOfUser(currentUser: User, view: View) {
+            val libraryLayout = view.findViewById<LinearLayout>(R.id.account_library)
+            libraryLayout.removeAllViewsInLayout()
+
+            LibraryArrayData.updateCompositions(currentUser)
+
+            val inflater = LayoutInflater.from(view.context)
+
+            for (i in LibraryArrayData.getCompositions()) {
                 val bookName = i.title
                 val bookAuthor = i.author
+                var bookStatus = ""
+                when {
+                    i.lib == "1" -> {
+                        bookStatus = "Прочитанное"
+                    }
+                    i.lib == "2" -> {
+                        bookStatus = "Отложенное"
+                    }
+                    i.lib == "3" -> {
+                        bookStatus = "Избранное"
+                    }
+                }
 
-                val removeButton = Button(view.context)
-                val paramsOfButton = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-                paramsOfButton.setMargins(10, 10, 0, 0)
-                removeButton.layoutParams = paramsOfTextView
-                removeButton.text = "Удалить из библиотеки"
-                removeButton.setOnClickListener { v ->
+                val libraryItemLayout = inflater.inflate(R.layout.fragment_library, null, false)
+                val imageView = libraryItemLayout.findViewById<ImageView>(R.id.library_image)
+                var urlImageView  = ""
+                for (book in BookArrayData.getBooks()) {
+                    if (book.composition.toString() == i.composition) {
+                        urlImageView = book.imageUrl
+                        break
+                    }
+                }
+                Picasso.get().load(urlImageView).placeholder(R.drawable.not_found).error(R.drawable.not_found)
+                        .fit().centerInside()
+                        .into(imageView)
+
+                libraryItemLayout.findViewById<TextView>(R.id.library_title).text = bookName
+                libraryItemLayout.findViewById<TextView>(R.id.library_author).text = bookAuthor
+                libraryItemLayout.findViewById<TextView>(R.id.library_status).text = bookStatus
+
+                libraryItemLayout.findViewById<Button>(R.id.library_remove).setOnClickListener { v ->
                     DelFromLibraryTask().execute(user.id.toString(), i.lib, i.composition)
                     updateLibraryOfUser(user, view)
                 }
-                when {
-                    i.lib == "1" -> {
-                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Прочитанное"
-                    }
-                    i.lib == "2" -> {
-                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Отложенное"
-                    }
-                    i.lib == "3" -> {
-                        newTextView.text = "Название: $bookName\nАвтор: $bookAuthor\nСтатус: Избранное"
-                    }
-                }
-                library.addView(newTextView)
-                library.addView(removeButton)
+
+                libraryLayout.addView(libraryItemLayout)
             }
             if (LibraryArrayData.getCompositions().size == 0) {
                 val newTextView = TextView(view.context)
@@ -84,7 +144,7 @@ class AccountFragment : Fragment() {
                 paramsOfTextView.setMargins(10, 10, 0, 0)
                 newTextView.layoutParams = paramsOfTextView
                 newTextView.text = "Пусто"
-                library.addView(newTextView)
+                libraryLayout.addView(newTextView)
             }
         }
 
